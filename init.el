6623c91ef5ce9b3f27ec5096be88c65adc4a9b2e
Jingkai Tang <jingkaitang@gmail.com>
2016-09-20 21:33:23 +0800

    mod: linum-mode, remove menu bar

--- init.el
+++ init.el
@@ -18,3 +18,19 @@
 (setq url-automatic-caching t)
 (global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point)
 (global-set-key (kbd "C-c t") 'youdao-dictionary-search-at-point+)
+
+(require 'simple)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(global-linum-mode t)
+ '(linum-format "%4d \u2502 ")
+ '(menu-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
