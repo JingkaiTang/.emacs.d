6c6843671dca5acbd61fd85028a0af9108c01ca2
Jingkai Tang <jingkaitang@gmail.com>
2016-09-20 15:12:33 +0800

    add: init.el

---
+++ init.el
@@ -0,0 +1,20 @@
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(setq package-list
+      '(youdao-dictionary))
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+;; youdao-dictionary
+(require 'youdao-dictionary)
+(setq url-automatic-caching t)
+(global-set-key (kbd "C-c y") 'youdao-dictionary-search-at-point)
+
